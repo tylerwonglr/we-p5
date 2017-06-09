bce8d5836cf5dc02e383bc2df4c125fbfe95b83d
Tyler Wong <tyler.wong@liferay.com>
2017-06-09 13:37:22 -0700

    updated gravity

--- src/js/render.js
+++ src/js/render.js
@@ -1,5 +1,6 @@
 import p5 from 'p5';
 import Player from './player';
+import Target from './target';
 import * as Colors from './colors';
 
 export default class Render {
@@ -9,23 +10,39 @@ export default class Render {
 		this.keyPressed = this.keyPressed.bind(this);
 
 		this.screen = {};
+
+		this.gravity = 5;
 	}
 
 	setup() {
 		createCanvas(windowWidth, windowHeight);
 
 		this.player = new Player(windowWidth / 2, windowHeight);
+
+		this.target = new Target(windowWidth / 2, 0);
 	}
 
 	draw() {
 		// background
 		background(Colors.BLUE);
 
+		// target
+		const {x: tx, y: ty, color: tcolor, radius: tradius} = this.target;
+
+		fill(tcolor);
+		noStroke();
+		ellipse(tx, ty, tradius, tradius);
+
 		// create player
 		const {x: px, y: py, color: pcolor, jumpHeight: pjumpHeight, radius: pradius} = this.player;
 
 		fill(pcolor);
 		ellipse(px, py, pradius, pradius);
+
+		// gravity
+		if (py <= windowHeight - this.gravity) {
+			this.player.y += this.gravity;
+		}
 
 		// etc.
 	}
--- src/js/target.js
+++ src/js/target.js
@@ -1,5 +1,10 @@
+import * as Colors from './colors';
+
 export default class Target {
 	constructor(x, y) {
-		this.position = {x, y};
+		this.x = x;
+		this.y = y;
+		this.color = Colors.RED;
+		this.radius = 50;
 	}
 }
