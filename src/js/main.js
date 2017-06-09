7129cc8c608c56b99a90d69e2d91ada6d7fcff07
Kien Do <kien.do@liferay.com>
2017-06-09 11:15:03 -0700

    remove commented out stuff

--- src/js/main.js
+++ src/js/main.js
@@ -2,21 +2,9 @@ import draw from './draw';
 
 const drawing = new draw();
 
-// var draw = require('./draw');
-
-// function setup() {
-// 	createCanvas(windowWidth, windowHeight);
-// }
-
 function main(window) {
 	window.setup = drawing.setup;
 	window.draw = drawing.draw;
 }
 
 main(window);
-
-// function draw() {
-// 	background(0);
-	
-// 	console.log('pew')
-// }
